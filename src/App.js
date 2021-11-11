5c478adad32140676adaea23ab6687110444808d
renaudmorin <40190695+renaudmorin@users.noreply.github.com>
2021-11-11 18:48:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
 import './App.css';
+import FormStep1 from './components/FormStep1';
+import { useState, useEffect} from "react";
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [formStep, setFormStep] = useState(0);
+  const [bpmMorning, setBpmMorning] = useState([]);
+  const [systoleMorning, setSystoleMorning] = useState([]);
+  const [diastoleMorning, setDiastoleMorning] = useState([]);
+  const [data, setData] = useState([
+    {day: 1, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 2, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 3, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 4, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 5, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 6, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 7, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 8, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 9, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 10, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 11, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 12, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 13, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+    {day: 14, diastoleMatin: null, systoleMatin: null, bpmMatin: null, diastoleSoir: null, systoleSoir: null, bpmSoir: null},
+  ])
+
+  function onDiastoleMatinChange(index, e) {
+    data[index].diastoleMatin = parseInt(e.target.value)
+  }
+
+  function onSystoleMatinChange(index, e) {
+    data[index].systoleMatin = parseInt(e.target.value)
+  }
+
+  function onBpmMatinChange(index, e) {
+    data[index].bpmMatin = parseInt(e.target.value)
+  }
+
+  function onDiastoleSoirChange(index, e) {
+    data[index].diastoleSoir = parseInt(e.target.value)
+  }
+
+  function onSystoleSoirChange(index, e) {
+    data[index].systoleSoir = parseInt(e.target.value)
+  }
+
+  function onBpmSoirChange(index, e) {
+    data[index].bpmSoir = parseInt(e.target.value)
+  }
+
+  if(formStep == 1) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <FormStep1 data={data} step={formStep}/>
+          <div className="buttonGroup">
+            <Button onClick={() => {
+              if(formStep === 0) {
+                return;
+              }
+              setFormStep(formStep - 1);
+            }} style={{backgroundColor: "white", width: "100px", color: "black", marginRight:"50px"}}>Back</Button>
+            <Button onClick={() => {
+              if(formStep >= 1) {
+                return;
+              }
+              setFormStep(formStep + 1);
+            }} style={{backgroundColor: "white", width: "100px", color: "black"}}>Next</Button>
+          </div>
+        </header>
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="center">diastole matin</TableCell>
+                    <TableCell align="center">systole matin</TableCell>
+                    <TableCell align="center">bpm matin</TableCell>
+                    <TableCell align="center">diastole soir</TableCell>
+                    <TableCell align="center">systole soir</TableCell>
+                    <TableCell align="center">bpm soir</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.map((row, i) => (
+                    <TableRow
+                      key={row.day}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row" style={{width: '100px'}}>
+                        Journée {row.day}
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField style={{width: '100px', height: '100px'}} onChange={(e) => onDiastoleMatinChange(i, e)} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField style={{width: '100px', height: '100px'}} onChange={(e) => onSystoleMatinChange(i, e)} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField style={{width: '100px', height: '100px'}} onChange={(e) => onBpmMatinChange(i, e)} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField style={{width: '100px', height: '100px'}} onChange={(e) => onDiastoleSoirChange(i, e)} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField style={{width: '100px', height: '100px'}} onChange={(e) => onSystoleSoirChange(i, e)} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField style={{width: '100px', height: '100px'}} onChange={(e) => onBpmSoirChange(i, e)} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        
+          <FormStep1 step= { formStep }/>
+          <div className="buttonGroup">
+            <Button onClick={() => {
+              if(formStep === 0) {
+                return;
+              }
+              setFormStep(formStep - 1);
+            }} style={{backgroundColor: "white", width: "100px", color: "black", marginRight:"50px"}}>Back</Button>
+            <Button onClick={() => {
+              setFormStep(formStep + 1);
+            }} style={{backgroundColor: "white", width: "100px", color: "black"}}>Next</Button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/FormStep1.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useState, useEffect} from "react";
+
+
+export default function FormStep1(props) {
+    const [diastoleAvg, setDiastoleAvg] = useState(0.0);
+    const [systoleAvg, setSystoleAvg] = useState(0.0);
+    const [bpmAvg, setBpmAvg] = useState(0.0);
+
+    function calculateTotals() {
+        if(props.data) {
+            let diastoleTotals = 0;
+            let systoleTotals = 0;
+            let bpmTotals = 0;
+            let diastoleCounts = 0;
+            let systoleCounts = 0;
+            let bpmCounts = 0;
+            props.data.map(element => {
+                if(element.diastoleMatin) {
+                    diastoleTotals += element.diastoleMatin;
+                    diastoleCounts += 1;
+                }
+                if(element.systoleMatin) {
+                    systoleTotals += element.systoleMatin;
+                    systoleCounts += 1;
+                }
+                if(element.bpmMatin) {
+                    bpmTotals += element.bpmMatin;
+                    bpmCounts += 1;
+                }
+                if(element.diastoleSoir) {
+                    diastoleTotals += element.diastoleSoir;
+                    diastoleCounts += 1;
+                }
+                if(element.systoleSoir) {
+                    systoleTotals += element.systoleSoir;
+                    systoleCounts += 1;
+                }
+                if(element.bpmSoir) {
+                    bpmTotals += element.bpmSoir;
+                    bpmCounts += 1;
+                }
+            })
+            setDiastoleAvg(diastoleTotals / diastoleCounts);
+            setSystoleAvg(systoleTotals / systoleCounts);
+            setBpmAvg(bpmTotals / bpmCounts);
+        }
+    }
+
+    useEffect(() => {
+        calculateTotals();
+
+    }, [props.data])
+
+    if(props.step === 1) {
+        return (
+            <div>
+                <span>Moyenne diastole: {diastoleAvg.toString()}</span> <br/>
+                <span>Moyenne systole: {systoleAvg.toString()}</span> <br/>
+                <span>Moyenne bpm: {bpmAvg.toString()}</span>
+            </div>
+        )
+    }
+    else {
+        return null;
+    }
+}
